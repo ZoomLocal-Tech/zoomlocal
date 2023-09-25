b9ee21824b672aafbe164aeca9b1217c0f48e24d
Anamika Yadav <aanyayadav419@gmail.com>
2023-09-25 15:13:49 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import HomePage from './components/HomePage.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HomePage msg="ZoomLocal" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  text-align: center;
+  padding: 10px;
+  padding-top: 2rem;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 5rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    align-items: flex-start;
+    top: 0;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green mb-3">{{ msg }}</h1>
+    <h2 class="mb-10">The next social frontier</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-row gap-4 justify-center">
+          <a href="https://uat-fly.web.app/" target="_blank" rel="noopener">
+            <div class="h-[170px] w-[170px]">
+              <img alt="Fly logo" class="logo cursor-pointer" src="@/assets/fly-logo.png" />
+            </div>
+          </a>
+        </div>
+        <h3 class="font-bold text-2xl">fly</h3>
+        <h3 class="tagline mb-4">Your social media steroids!</h3>
+      </div>
+      <div class="flex flex-col gap-4 pb-4">
+        <div class="flex flex-row gap-4 justify-center">
+          <a href="https://goaround.me/" target="_blank" rel="noopener">
+            <div class="h-[170px] w-[170px]">
+              <img
+                alt="Fly logo"
+                class="logo cursor-pointer h-5/6 mx-auto relative top-[-15px]"
+                src="@/assets/around-logo.png"
+              />
+            </div>
+          </a>
+        </div>
+        <h3 class="font-bold text-2xl">around</h3>
+        <h3 class="tagline mb-1">Your superpower to discover and connect with people around!</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 600;
+  font-size: 2.6rem;
+  position: relative;
+}
+
+h2 {
+  font-size: 1.5rem;
+  font-weight: 500;
+}
+
+h3.tagline {
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+</style>
