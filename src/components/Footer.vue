4d99ef454deefbea6755021f52d986f415589e8b
Anamika Yadav <aanyayadav419@gmail.com>
2023-09-26 09:28:48 +0530

    changes to title, footer, tagline

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="min-h-[15vh] w-full relative p-8">
     <div
-      class="footer-content flex items-center absolute justify-center left-1/2 bottom-8 text-base translate-x-[-50%]"
+      class="footer-content flex items-center absolute justify-center left-1/2 bottom-8 text-sm w-full sm:text-base translate-x-[-50%]"
     >
       <p>@2023 ZoomLocal Tech Private Limited, India</p>
     </div>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -10,8 +10,8 @@ defineProps({
 <template>
   <div class="greetings min-h-[75vh]">
     <h1 class="green mb-2">{{ msg }}</h1>
-    <h2 class="mb-28">The next social frontier</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-10 md:gap-32">
+    <h2 class="mb-16 sm:mb-28">The next social frontier</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-10 md:gap-32">
       <div class="flex flex-col items-center">
         <a
           class="flex flex-col gap-4 justify-center"
@@ -22,9 +22,16 @@ defineProps({
           <div class="h-[120px] w-[160px]">
             <img alt="Fly logo" class="logo cursor-pointer" src="@/assets/fly-logo.png" />
           </div>
-          <h3 class="font-bold text-2xl mb-3 text-blue-600 hover:text-blue-800">fly</h3>
+          <h3 class="font-bold text-2xl mb-3 text-gray-700 hover:text-blue-800">fly</h3>
         </a>
-        <h3 class="tagline mb-4">Your social media steroids!</h3>
+        <h3 class="tagline mb-4">
+          Your social media steroids!
+          <a href="https://uat-fly.web.app/" target="_blank" rel="noopener"
+            ><span class="text-base text-blue-800 font-medium leading-4 cursor-pointer"
+              >Visit</span
+            ></a
+          >
+        </h3>
       </div>
       <div class="flex flex-col items-center pb-4">
         <a
@@ -36,15 +43,20 @@ defineProps({
           <div class="h-[120px] w-[170px]">
             <img
               alt="Fly logo"
-              class="logo cursor-pointer h-5/6 mx-auto relative sm:top-[-15px]"
+              class="logo cursor-pointer h-[107%] mx-auto relative sm:top-[-17px]"
               src="@/assets/around-logo.png"
             />
           </div>
-          <h3 class="font-bold text-2xl mb-3 text-blue-600 hover:text-blue-800">around</h3>
+          <h3 class="font-bold text-2xl mb-3 text-gray-700 hover:text-blue-800">around</h3>
         </a>
         <h3 class="tagline mb-1">
           Your superpower to discover and <br />
           connect with people around!
+          <a href="https://goaround.me/" target="_blank" rel="noopener"
+            ><span class="text-base text-blue-800 font-medium leading-4 cursor-pointer"
+              >Visit</span
+            ></a
+          >
         </h3>
       </div>
     </div>
@@ -64,7 +76,7 @@ h2 {
 }
 
 h3.tagline {
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   font-weight: 500;
 }
 </style>
